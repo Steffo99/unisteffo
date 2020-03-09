a6aaeb0e7bf6bdc5c027bc6db6da7daaa259afe9
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-03-10 00:18:13 +0100

    Update

---
+++ docs/precache-manifest.af1d9b57e7e1a5734e84113f4f9dfa71.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b714cf154cd80b3949545dfe0c5939a",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "93e409e8dd1d3de29398",
+    "url": "/bundle.93e40.js"
+  },
+  {
+    "revision": "93e409e8dd1d3de29398",
+    "url": "/bundle.99dfc.css"
+  },
+  {
+    "revision": "c28c3ac3eb77ff39d980956e686b4a69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb23adb3c9bfbb7f047f",
+    "url": "/polyfills.bb23a.js"
+  },
+  {
+    "revision": "0e122694e48ffd25968a",
+    "url": "/route-fisica.chunk.0e122.js"
+  },
+  {
+    "revision": "0e122694e48ffd25968a",
+    "url": "/route-fisica.chunk.73001.css"
+  },
+  {
+    "revision": "da4e0543461a47137511",
+    "url": "/route-home.chunk.da4e0.js"
+  },
+  {
+    "revision": "da4e0543461a47137511",
+    "url": "/route-home.chunk.dcd89.css"
+  },
+  {
+    "revision": "5aa37a94577329288078",
+    "url": "/route-mingwinstall.chunk.4f81b.css"
+  },
+  {
+    "revision": "5aa37a94577329288078",
+    "url": "/route-mingwinstall.chunk.5aa37.js"
+  },
+  {
+    "revision": "d2a678f06e6e67b50a5c",
+    "url": "/route-ottimizzazioneLineare.chunk.afc61.css"
+  },
+  {
+    "revision": "d2a678f06e6e67b50a5c",
+    "url": "/route-ottimizzazioneLineare.chunk.d2a67.js"
+  },
+  {
+    "revision": "cbc10af8ba24b0503215",
+    "url": "/route-statistica.chunk.75621.css"
+  },
+  {
+    "revision": "cbc10af8ba24b0503215",
+    "url": "/route-statistica.chunk.cbc10.js"
+  },
+  {
+    "revision": "4396d4266c312fde4ad3",
+    "url": "/route-vldigeometria.chunk.4396d.js"
+  },
+  {
+    "revision": "4396d4266c312fde4ad3",
+    "url": "/route-vldigeometria.chunk.e54c2.css"
+  },
+  {
+    "revision": "13b82ed03d648451c57be206f75f0216",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "f6b295cedfe1a0f96edd7d67d665be4a",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
---
+++ src/routes/ottimizzazioneLineare.js
@@ -0,0 +1,12 @@
+import {Component} from 'preact'
+import style from './ottimizzazioneLineare.less'
+
+export default class OttimizzazioneLineare extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
