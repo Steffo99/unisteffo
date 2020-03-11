4aae7978e46e51ba58e159d7e2f01ad1e7391560
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-03-11 19:23:38 +0100

    Use preact-cli 3.0.0-rc.10

---
+++ docs/precache-manifest.6fce2a3fd4d0897e853c293b267b95c6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b714cf154cd80b3949545dfe0c5939a",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "c755a4a1ec597a48ca92",
+    "url": "/bundle.69171.css"
+  },
+  {
+    "revision": "c755a4a1ec597a48ca92",
+    "url": "/bundle.c755a.js"
+  },
+  {
+    "revision": "4c6033fdcae6128844ba8495bfe5993e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96b28270e3dc7238eca",
+    "url": "/polyfills.b96b2.js"
+  },
+  {
+    "revision": "b1ca45d0b54b1b72bb0f",
+    "url": "/route-apprendimentoSistemiArtificiali.chunk.b1ca4.js"
+  },
+  {
+    "revision": "b1ca45d0b54b1b72bb0f",
+    "url": "/route-apprendimentoSistemiArtificiali.chunk.c2edb.css"
+  },
+  {
+    "revision": "d1ab5ce994cf531c3f6a",
+    "url": "/route-basiDiDati.chunk.c2edb.css"
+  },
+  {
+    "revision": "d1ab5ce994cf531c3f6a",
+    "url": "/route-basiDiDati.chunk.d1ab5.js"
+  },
+  {
+    "revision": "a852daff915a3d8d415c",
+    "url": "/route-calcoloNumerico.chunk.a852d.js"
+  },
+  {
+    "revision": "a852daff915a3d8d415c",
+    "url": "/route-calcoloNumerico.chunk.c2edb.css"
+  },
+  {
+    "revision": "3f6c6e546e25cd8e9a13",
+    "url": "/route-fisica.chunk.0b01a.css"
+  },
+  {
+    "revision": "3f6c6e546e25cd8e9a13",
+    "url": "/route-fisica.chunk.3f6c6.js"
+  },
+  {
+    "revision": "65775d988715d1bf69da",
+    "url": "/route-home.chunk.65775.js"
+  },
+  {
+    "revision": "65775d988715d1bf69da",
+    "url": "/route-home.chunk.deccf.css"
+  },
+  {
+    "revision": "9d6e98cebdfe11f3b294",
+    "url": "/route-mingwinstall.chunk.4f81b.css"
+  },
+  {
+    "revision": "9d6e98cebdfe11f3b294",
+    "url": "/route-mingwinstall.chunk.9d6e9.js"
+  },
+  {
+    "revision": "56a70e4ff392b90349dc",
+    "url": "/route-ottimizzazioneLineare.chunk.56a70.js"
+  },
+  {
+    "revision": "56a70e4ff392b90349dc",
+    "url": "/route-ottimizzazioneLineare.chunk.c2edb.css"
+  },
+  {
+    "revision": "fe1f84186d58ae6bb2b1",
+    "url": "/route-statistica.chunk.025c9.css"
+  },
+  {
+    "revision": "fe1f84186d58ae6bb2b1",
+    "url": "/route-statistica.chunk.fe1f8.js"
+  },
+  {
+    "revision": "3b8b97318793e55a269a",
+    "url": "/route-vldigeometria.chunk.3b8b9.js"
+  },
+  {
+    "revision": "3b8b97318793e55a269a",
+    "url": "/route-vldigeometria.chunk.e54c2.css"
+  },
+  {
+    "revision": "144c6ff1323d2d15182a7d2af08d027e",
+    "url": "/sw-debug.js"
+  },
+  {
+    "revision": "448c4ae24730f36c1873409f97ef5147",
+    "url": "/sw-esm.js"
+  },
+  {
+    "revision": "bfc5bd18885255982238f3628770f48d",
+    "url": "/sw.js"
+  }
+]);
---
+++ docs/precache-manifest.71153f24f745bfd6b92dd5e2b93127cc.esm.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b714cf154cd80b3949545dfe0c5939a",
+    "url": "/assets/icon.png"
+  },
+  {
+    "revision": "c755a4a1ec597a48ca92",
+    "url": "/bundle.5f7c2.esm.js"
+  },
+  {
+    "revision": "c755a4a1ec597a48ca92",
+    "url": "/bundle.69171.css"
+  },
+  {
+    "revision": "4c6033fdcae6128844ba8495bfe5993e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b96b28270e3dc7238eca",
+    "url": "/polyfills.7484f.esm.js"
+  },
+  {
+    "revision": "b1ca45d0b54b1b72bb0f",
+    "url": "/route-apprendimentoSistemiArtificiali.chunk.c2edb.css"
+  },
+  {
+    "revision": "b1ca45d0b54b1b72bb0f",
+    "url": "/route-apprendimentoSistemiArtificiali.chunk.fdc4f.esm.js"
+  },
+  {
+    "revision": "d1ab5ce994cf531c3f6a",
+    "url": "/route-basiDiDati.chunk.ac797.esm.js"
+  },
+  {
+    "revision": "d1ab5ce994cf531c3f6a",
+    "url": "/route-basiDiDati.chunk.c2edb.css"
+  },
+  {
+    "revision": "a852daff915a3d8d415c",
+    "url": "/route-calcoloNumerico.chunk.5f4f3.esm.js"
+  },
+  {
+    "revision": "a852daff915a3d8d415c",
+    "url": "/route-calcoloNumerico.chunk.c2edb.css"
+  },
+  {
+    "revision": "3f6c6e546e25cd8e9a13",
+    "url": "/route-fisica.chunk.0b01a.css"
+  },
+  {
+    "revision": "3f6c6e546e25cd8e9a13",
+    "url": "/route-fisica.chunk.4ed8c.esm.js"
+  },
+  {
+    "revision": "65775d988715d1bf69da",
+    "url": "/route-home.chunk.86a07.esm.js"
+  },
+  {
+    "revision": "65775d988715d1bf69da",
+    "url": "/route-home.chunk.deccf.css"
+  },
+  {
+    "revision": "9d6e98cebdfe11f3b294",
+    "url": "/route-mingwinstall.chunk.4f81b.css"
+  },
+  {
+    "revision": "9d6e98cebdfe11f3b294",
+    "url": "/route-mingwinstall.chunk.ea3ae.esm.js"
+  },
+  {
+    "revision": "56a70e4ff392b90349dc",
+    "url": "/route-ottimizzazioneLineare.chunk.c2edb.css"
+  },
+  {
+    "revision": "56a70e4ff392b90349dc",
+    "url": "/route-ottimizzazioneLineare.chunk.d97a3.esm.js"
+  },
+  {
+    "revision": "fe1f84186d58ae6bb2b1",
+    "url": "/route-statistica.chunk.025c9.css"
+  },
+  {
+    "revision": "fe1f84186d58ae6bb2b1",
+    "url": "/route-statistica.chunk.26925.esm.js"
+  },
+  {
+    "revision": "3b8b97318793e55a269a",
+    "url": "/route-vldigeometria.chunk.24244.esm.js"
+  },
+  {
+    "revision": "3b8b97318793e55a269a",
+    "url": "/route-vldigeometria.chunk.e54c2.css"
+  }
+]);
