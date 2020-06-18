ffd51732bc0bd419fd6f426a76df7f233f1b6b88
Stefano Pigozzi <ste.pigozzi@gmail.com>
2020-06-18 15:39:16 +0200

    It works! (0.9.4)

--- src/components/Errors/Error.js
+++
@@ -1,9 +0,0 @@
-import Box, {BoxColors} from "../Elements/Box";
-
-export default function(props) {
-    return (
-        <Box color={BoxColors.RED}>
-            {props.children}
-        </Box>
-    );
-}
