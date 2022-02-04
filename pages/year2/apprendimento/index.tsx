da1a7b7f6a0d8c7653d2a9a5b10612ac6ac0055e
Stefano Pigozzi <me@steffo.eu>
2022-02-04 18:59:42 +0100

    add warning to apprendimento

--- pages/year2/apprendimento/index.tsx
+++ pages/year2/apprendimento/index.tsx
@@ -2,6 +2,7 @@ import { ListUnordered } from "@steffo/bluelib-react"
 import { Heading, Chapter, Box, Panel, Idiomatic as I } from "@steffo/bluelib-react"
 import { NextPage } from "next"
 import { Link } from "../../../components/link"
+import { Warn1024 } from "../../../components/warn"
 
 
 const Page: NextPage = () => {
@@ -32,6 +33,7 @@ const Page: NextPage = () => {
                         <Heading level={4}>
                             Appuntiweb
                         </Heading>
+                        <Warn1024/>
                         <ListUnordered>
                             <ListUnordered.Item>
                                 <Link href="/year2/apprendimento/teoria">
