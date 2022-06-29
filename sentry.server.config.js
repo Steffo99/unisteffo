79148db0916ce271360b9b1b4c86a907f21e4f28
Stefano Pigozzi <me@steffo.eu>
2022-06-29 09:21:47 +0200

    Hide Sentry DSN in a envvar

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -4,10 +4,10 @@
 
 import * as Sentry from '@sentry/nextjs';
 
-const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const dsn = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: "https://8ff38639de2c49f39773a49864c6e775@o40131.ingest.sentry.io/6185233",
+  dsn,
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
