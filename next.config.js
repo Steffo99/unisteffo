79a77d51d15f6117064babe4b4b26efa879530a0
Stefano Pigozzi <me@steffo.eu>
2023-03-15 09:06:55 +0000

    Indent with 4 spaces

--- next.config.js
+++ next.config.js
@@ -8,26 +8,26 @@
 const { withSentryConfig } = require('@sentry/nextjs');
 
 const moduleExports = {
-  reactStrictMode: true,
-  sentry: {
-    hideSourceMaps: false,
-  }
+    reactStrictMode: true,
+    sentry: {
+        hideSourceMaps: false,
+    },
 };
 
 const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
+    // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    // the following options are set automatically, and overriding them is not
+    // recommended:
+    //   release, url, org, project, authToken, configFile, stripPrefix,
+    //   urlPrefix, include, ignore
 
-  url: "https://sentry.io/",
-  org: "stefano-pigozzi",
-  project: "unisteffo",
+    url: "https://sentry.io/",
+    org: "stefano-pigozzi",
+    project: "unisteffo",
 
-  silent: true, // Suppresses all logs
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
+    silent: true, // Suppresses all logs
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options.
 };
 
 // Make sure adding Sentry options is the last code to run before exporting, to
